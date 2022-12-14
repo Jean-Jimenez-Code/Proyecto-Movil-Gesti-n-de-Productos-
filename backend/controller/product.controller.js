13c72765e527d747fb785b7c31032a5430faa704
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2022-12-14 14:30:13 -0300

    .

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -4,14 +4,13 @@ const getProducts = async(req , res)=>{
     const products=await prodSchema.find()
     console.log('hola')
     console.log(products)
-
     res.json(products)
 }
 
 const postProduct=async (req,res)=>{
-       const body =  req.body
-       const product = await prodSchema.create(body)
-       console.log(product)
-       res.json(product)
+    const item =  req.body;
+    const product = await prodSchema.create(item)
+       
+    res.json(product)     
 }
 module.exports={getProducts,postProduct}
